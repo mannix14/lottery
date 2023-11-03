05840c074adcf16dc26c47a00f891cb71cd8cdfe
Mannix <95986302+mannix14@users.noreply.github.com>
2023-11-03 10:57:52 -0400

    update js
    updated random numbers to fit the range of numbers from the last 100 drawings.

--- index.js
+++ index.js
@@ -9,22 +9,22 @@
 		var six= document.getElementById('six');
 
 		//CREATE A RANDOM NUMBER FOR EACH OF THE CIRCLES
-		var ball1 = Math.floor(Math.random() * 21) +1;
-		var ball2 = Math.floor(Math.random() * 31) + 20;
-		var ball3 = Math.floor(Math.random() * 70);
-		var ball4 = Math.floor(Math.random() * 70);
-		var ball5 = Math.floor(Math.random() * 30) + 40;
+		var ball1 = Math.floor(Math.random() * 29) + 1;
+		var ball2 = Math.floor(Math.random() * 46) + 2;
+		var ball3 = Math.floor(Math.random() * 47) + 11;
+		var ball4 = Math.floor(Math.random() * 48) + 22;
+		var ball5 = Math.floor(Math.random() * 29) + 40;
 		var powerBall = Math.floor(Math.random() * 26) + 1;
 
 			//CHECK FOR DUPLICATE
 			if (ball2 === ball1 || ball2 === ball3 || ball2 === ball4 || ball2 === ball5) {
 			console.log("duplicate with 2nd ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
-			var ball2 = Math.floor(Math.random() * 31) + 30;
+			var ball2 = Math.floor(Math.random() * 46) + 2;
 				return ball2
 		}
 		else if (ball3 === ball1 || ball3 === ball4 || ball3 === ball5) {
 			console.log("duplicate with 3rd ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
-			var ball3 = Math.floor(Math.random() * 31) + 30;
+			var ball3 = Math.floor(Math.random() * 47) + 11;
 			return ball3
 				}
 		else if (ball4 === ball1 || ball4 === ball1) {
