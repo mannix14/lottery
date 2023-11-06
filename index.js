51fb514cfffec51a0753aa75371cc030bc486066
Mannix <95986302+mannix14@users.noreply.github.com>
2023-11-06 10:57:54 -0500

    clean up

--- index.js
+++ index.js
@@ -13,36 +13,38 @@
 		var ball3 = Math.floor(Math.random() * 47) + 11;
 		var ball4 = Math.floor(Math.random() * 48) + 21;
 		var ball5 = Math.floor(Math.random() * 29) + 40;
-		// console.log(ball5)
 		var powerBall = Math.floor(Math.random() * 26) + 1;
 
 			//CHECK FOR DUPLICATE
-			if (ball2 === ball1 || ball2 === ball3 || ball2 === ball4 || ball2 === ball5) {
-			console.log("duplicate with 2nd ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
-			var ball2 = Math.floor(Math.random() * 46) + 2;
-				return ball2
-		}
-		else if (ball3 === ball1 || ball3 === ball4 || ball3 === ball5) {
-			console.log("duplicate with 3rd ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
-			var ball3 = Math.floor(Math.random() * 47) + 11;
-			return ball3
-				}
-		else if (ball4 === ball3 || ball4 === ball5) {
-			console.log("duplicate with 4th ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
-				var ball4 = Math.floor(Math.random() * 28) + 21;
-			return ball4
-			}
-				else if (ball5 === ball1 || ball5 === ball2 || ball5 === ball3 || ball5 === ball4 || ball5 < 40) {
-			console.log("duplicate with 4th ball " + ball1 + "," + ball2 + "," + ball3 +","  + ball4 +","  + ball5)
+
+				if (ball5 === ball1 || ball5 === ball2 || ball5 === ball3 || ball5 === ball4 || ball5 < 40) {
 				var ball5 = Math.floor(Math.random() * 29) + 40;
 			return ball5
 		}
+					else if (ball4 === ball3 || ball4 === ball5) {
+				var ball4 = Math.floor(Math.random() * 48) + 21;
+			return ball4
+			}
+					else if (ball3 === ball1 || ball3 === ball4 || ball3 === ball5) {
+			var ball3 = Math.floor(Math.random() * 47) + 11;
+			return ball3
+				}
+						else	if (ball2 === ball1 || ball2 === ball3 || ball2 === ball4 || ball2 === ball5) {
+			var ball2 = Math.floor(Math.random() * 46) + 2;
+				return ball2
+			}
+
+				else if (ball1 === ball2 || ball1 === ball3 || ball1 === ball4  || ball1 === ball5 || ball1 > 29) {
+			var ball1 = Math.floor(Math.random() * 29) + 1;
+			return ball1
+			}
+
 
 		//SORT AND LOOP
 		 const balls = [ball1, ball2, ball3, ball4, ball5];
 		 balls.sort(function (a, b) { return a - b });
 
-			console.log(balls)
+		console.log(balls)
 
 //INSERT INTO BALLS
 	one.textContent = balls[0];
